c500e6fa9382c6b2c2424953a470ab58c93a8de5
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2021-07-04 11:14:17 +0200

    expose control plane metrics to prometheus - closes #373
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>
    Change-Id: I361dd66eea5409344d1745c4231d2ebf60664e01
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>

--- tests/.stage2_default/verify.d/test_k8s.py
+++ tests/.stage2_default/verify.d/test_k8s.py
@@ -1,4 +1,5 @@
 import time
+import json
 kubectl = {}
 def kubectl_cmd(host, cmd):
     if not kubectl.get((host,cmd)):
@@ -26,3 +27,8 @@ def test_metallb_service_ip(host):
     kubectl_cmd(host, "delete service test-service")
     assert service_ip.startswith("127.5.0")
 
+def test_metallb_service_ip(host):
+    service_ip = kubectl_cmd(host, "-n monitoring get services kube-prometheus-prometheus -o jsonpath='{.spec.clusterIP}'")['stdout']
+    output = json.loads(host.check_output('curl -s "{service_ip}:9090/api/v1/query?query=up"'.format(service_ip=service_ip)))
+    for i in filter(lambda x: x['metric']['service'] in ["kube-prometheus-kube-controller-manager", "kube-prometheus-node-exporter", "kube-prometheus-kube-scheduler"], output['data']['result']):
+        assert i['value'][1] == '1', "%s target is down"%i['metric']['service']
